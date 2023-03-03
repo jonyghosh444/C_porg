145ad28b81ee3740e59c4534aa17662b16fed6ed
Jony ghosh <jony.ghosh.jgh@gmail.com>
2023-03-03 22:29:40 +0600

    created 2D_array_c.c

---
+++ 2D_array_c.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int arr[2][4] = {{34, 2, 1, 454}, {33, 22, 31, 34}};
+
+    for (int i = 0; i < 2; i++)
+    {
+        for (int j = 0; j < 4; j++)
+        {
+            printf("%d ", arr[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
